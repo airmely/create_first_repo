672f12740950926e43afbf8ab6ca24a3b1d57361
Melkonyan Airenik <swenovish@gmail.com>
2023-02-09 22:14:27 +0300

    Быстрая сортировка, почему бы и нет

--- pprint_file.py
+++ pprint_file.py
@@ -1,1 +1,13 @@
 print('This is a file from GitHub repository')
+
+
+def quick_sort(lst: list):
+    if len(lst) <= 1:
+        return lst
+
+    return quick_sort([i for i in lst[1:] if i < lst[0]])\
+        + [lst[0]]\
+        + quick_sort([i for i in lst[1:] if i >= lst[0]])
+
+
+print(quick_sort([-6, 1, 2]))
